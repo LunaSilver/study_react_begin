8d64be4c64f87c45a6e2cca272d775cf998a1d69
Luna <yeon@catbus.xyz>
2021-12-08 00:05:12 +0900

    first commit

---
+++ src/routes/Detail.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+function Detail() {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState({});
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <a href={movie.url}>
+            <h2>{movie.title_long}</h2>
+          </a>
+          <h4>{movie.rating}</h4>
+          <p>{movie.description_intro}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Detail;
